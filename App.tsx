7bcfc6ac7b1df1e4fb5ffbf1c43b262742f77834
MudMoh <mudesir@gmail.com>
2025-11-30 03:01:58 +0300

    fix: remove expo-localization import from App.tsx

--- App.tsx
+++ App.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { I18nManager } from 'react-native';
-import * as RNLocalize from 'expo-localization';
 import { JobProvider } from './src/context/JobContext';
 import HomeScreen from './src/screens/HomeScreen';
 import JobDetailsScreen from './src/screens/JobDetailsScreen';
 import ApplyScreen from './src/screens/ApplyScreen';
 import { Job } from './src/types/job';
 
-// Set RTL for Arabic
-const isRTL = RNLocalize.getLocales()[0].languageCode === 'ar';
-I18nManager.allowRTL(isRTL);
-I18nManager.forceRTL(isRTL);
 
 type RootStackParamList = {
   Home: undefined;
